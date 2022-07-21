1cd74a44e44b02cabf0918ca894e9ba6c4391822
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-07-21 18:34:16 +0300

    changes to the category route screen

--- lib/category_route.dart
+++ lib/category_route.dart
@@ -1,13 +1,14 @@
-
-
 // Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:demo/category.dart';
+import 'package:demo/unit.dart';
 import 'package:flutter/material.dart';
 
-//import 'package:solution_03_category_route/category.dart';
+import 'category.dart';
+
+// import 'package:task_05_stateful_widgets/category.dart';
+// import 'package:task_05_stateful_widgets/unit.dart';
 
 final _backgroundColor = Colors.green[100];
 
@@ -18,8 +19,11 @@ final _backgroundColor = Colors.green[100];
 ///
 /// While it is named CategoryRoute, a more apt name would be CategoryScreen,
 /// because it is responsible for the UI at the route's destination.
+// TODO: Make CategoryRoute a StatefulWidget
 class CategoryRoute extends StatelessWidget {
   const CategoryRoute({Key? key}) : super(key: key);
+
+  // TODO: Create State object for the CategoryRoute
 
   static const _categoryNames = <String>[
     'Length',
@@ -45,7 +49,7 @@ class CategoryRoute extends StatelessWidget {
 
   /// Makes the correct number of rows for the list view.
   ///
-  /// For portrait, we construct a [ListView] from the list of category widgets.
+  /// For portrait, we use a [ListView].
   Widget _buildCategoryWidgets(List<Widget> categories) {
     return ListView.builder(
       itemBuilder: (BuildContext context, int index) => categories[index],
@@ -53,8 +57,24 @@ class CategoryRoute extends StatelessWidget {
     );
   }
 
+  /// Returns a list of mock [Unit]s.
+  List<Unit> _retrieveUnitList(String categoryName) {
+    return List.generate(10, (int i) {
+      i += 1;
+      return Unit(
+        name: '$categoryName Unit $i',
+        conversion: i.toDouble(),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    // TODO: Instead of re-creating a list of Categories in every build(),
+    // save this as a variable inside the State object and create
+    // the list at initialization (in initState()).
+    // This way, you also don't have to pass in the list of categories to
+    // _buildCategoryWidgets()
     final categories = <Category>[];
 
     for (var i = 0; i < _categoryNames.length; i++) {
@@ -62,6 +82,7 @@ class CategoryRoute extends StatelessWidget {
         name: _categoryNames[i],
         color: _baseColors[i] as ColorSwatch<dynamic>,
         iconLocation: Icons.cake,
+        units: _retrieveUnitList(_categoryNames[i]),
       ));
     }
 
