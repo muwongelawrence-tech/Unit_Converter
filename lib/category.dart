d02d651ef7267158d517499bf5b15f3388c8e239
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-07-21 18:22:48 +0300

    Adding Routing to the application

--- lib/category.dart
+++ lib/category.dart
@@ -4,7 +4,13 @@
 
 // To keep your imports tidy, follow the ordering guidelines at
 // https://www.dartlang.org/guides/language/effective-dart/style#ordering
+import 'package:demo/unit.dart';
 import 'package:flutter/material.dart';
+
+// import 'package:task_05_stateful_widgets/converter_route.dart';
+// import 'package:task_05_stateful_widgets/unit.dart';
+
+import 'converter_route.dart';
 
 // We use an underscore to indicate that these variables are private.
 // See https://www.dartlang.org/guides/language/effective-dart/design#libraries
@@ -19,6 +25,7 @@ class Category extends StatelessWidget {
   final String name;
   final ColorSwatch color;
   final IconData iconLocation;
+  final List<Unit> units;
 
   /// Creates a [Category].
   ///
@@ -32,7 +39,31 @@ class Category extends StatelessWidget {
     required this.name,
     required this.color,
     required this.iconLocation,
+    required this.units,
   }) : super(key: key);
+
+  /// Navigates to the [ConverterRoute].
+  void _navigateToConverter(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute<void>(
+      builder: (BuildContext context) {
+        return Scaffold(
+          appBar: AppBar(
+            elevation: 1.0,
+            title: Text(
+              name,
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            centerTitle: true,
+            backgroundColor: color,
+          ),
+          body: ConverterRoute(
+            color: color,
+            units: units,
+          ),
+        );
+      },
+    ));
+  }
 
   /// Builds a custom widget that shows [Category] information.
   ///
@@ -53,9 +84,7 @@ class Category extends StatelessWidget {
           splashColor: color,
           // We can use either the () => function() or the () { function(); }
           // syntax.
-          onTap: () {
-            print('I was tapped!');
-          },
+          onTap: () => _navigateToConverter(context),
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Row(
--- lib/main.dart
+++ lib/main.dart
@@ -31,4 +31,3 @@ class UnitConverterApp extends StatelessWidget {
     );
   }
 }
-
