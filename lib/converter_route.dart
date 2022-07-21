5c2563f794c4051d69707f4ea2351b85c2f0aa3c
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-07-21 18:10:56 +0300

    adding converter_router

---
+++ lib/converter_route.dart
@@ -0,0 +1,62 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:demo/unit.dart';
+import 'package:flutter/material.dart';
+
+//import 'package:task_05_stateful_widgets/unit.dart';
+
+/// Converter screen where users can input amounts to convert.
+///
+/// Currently, it just displays a list of mock units.
+///
+/// While it is named ConverterRoute, a more apt name would be ConverterScreen,
+/// because it is responsible for the UI at the route's destination.
+// TODO: Make ConverterRoute a StatefulWidget
+class ConverterRoute extends StatelessWidget {
+  /// Color for this [Category].
+  final Color color;
+
+  /// Units for this [Category].
+  final List<Unit> units;
+
+  /// This [ConverterRoute] requires the color and units to not be null.
+  const ConverterRoute({
+    required this.color,
+    required this.units,
+    Key? key,
+  }) : super(key: key);
+
+  // TODO: Create State object for the ConverterRoute
+
+  @override
+  Widget build(BuildContext context) {
+    // Here is just a placeholder for a list of mock units
+    // TODO: Once the build() function is inside the State object,
+    // you'll have to reference this using `widget.units`
+    final unitWidgets = units.map((Unit unit) {
+      return Container(
+        color: color,
+        margin: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Text(
+              unit.name!,
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            Text(
+              'Conversion: ${unit.conversion}',
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+          ],
+        ),
+      );
+    }).toList();
+
+    return ListView(
+      children: unitWidgets,
+    );
+  }
+}
