fe088bed384e5f035cfa4b55822e40a6066ec40e
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-07-22 19:17:01 +0300

    updates

--- lib/converter_route.dart
+++ lib/converter_route.dart
@@ -2,19 +2,20 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:demo/unit.dart';
+// ignore_for_file: unused_element
+
 import 'package:flutter/material.dart';
 
-//import 'package:task_05_stateful_widgets/unit.dart';
+import 'unit.dart';
 
-/// Converter screen where users can input amounts to convert.
-///
-/// Currently, it just displays a list of mock units.
+const _padding = EdgeInsets.all(16.0);
+
+/// [ConverterRoute] where users can input amounts to convert in one [Unit]
+/// and retrieve the conversion in another [Unit] for a specific [Category].
 ///
 /// While it is named ConverterRoute, a more apt name would be ConverterScreen,
 /// because it is responsible for the UI at the route's destination.
-// TODO: Make ConverterRoute a StatefulWidget
-class ConverterRoute extends StatelessWidget {
+class ConverterRoute extends StatefulWidget {
   /// Color for this [Category].
   final Color color;
 
@@ -28,16 +29,50 @@ class ConverterRoute extends StatelessWidget {
     Key? key,
   }) : super(key: key);
 
-  // TODO: Create State object for the ConverterRoute
+  @override
+  _ConverterRouteState createState() => _ConverterRouteState();
+}
+
+class _ConverterRouteState extends State<ConverterRoute> {
+  // TODO: Set some variables, such as for keeping track of the user's input
+  // value and units
+
+  // TODO: Determine whether you need to override anything, such as initState()
+
+  // TODO: Add other helper functions. We've given you one, _format()
+
+  /// Clean up conversion; trim trailing zeros, e.g. 5.500 -> 5.5, 10.0 -> 10
+  String _format(double conversion) {
+    var outputNum = conversion.toStringAsPrecision(7);
+    if (outputNum.contains('.') && outputNum.endsWith('0')) {
+      var i = outputNum.length - 1;
+      while (outputNum[i] == '0') {
+        i -= 1;
+      }
+      outputNum = outputNum.substring(0, i + 1);
+    }
+    if (outputNum.endsWith('.')) {
+      return outputNum.substring(0, outputNum.length - 1);
+    }
+    return outputNum;
+  }
 
   @override
   Widget build(BuildContext context) {
-    // Here is just a placeholder for a list of mock units
-    // TODO: Once the build() function is inside the State object,
-    // you'll have to reference this using `widget.units`
-    final unitWidgets = units.map((Unit unit) {
+    // TODO: Create the 'input' group of widgets. This is a Column that
+    // includes the input value, and 'from' unit [Dropdown].
+
+    // TODO: Create a compare arrows icon.
+
+    // TODO: Create the 'output' group of widgets. This is a Column that
+    // includes the output value, and 'to' unit [Dropdown].
+
+    // TODO: Return the input, arrows, and output widgets, wrapped in a Column.
+
+    // TODO: Delete the below placeholder code.
+    final unitWidgets = widget.units.map((Unit unit) {
       return Container(
-        color: color,
+        color: widget.color,
         margin: const EdgeInsets.all(8.0),
         padding: const EdgeInsets.all(16.0),
         child: Column(
