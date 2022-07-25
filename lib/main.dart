8e3768d671105e2f0af59c6f017c7791a64fe524
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-07-25 17:30:47 +0300

    configuring category

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@
 import 'package:flutter/material.dart';
 
 // You can use a relative import, i.e. `import 'category_route.dart;'` or
-// a package import, as shown below.
+// a package import.
 // More details at http://dart-lang.github.io/linter/lints/avoid_relative_lib_imports.html
 import 'category_route.dart';
 
@@ -24,11 +24,20 @@ class UnitConverterApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Unit Converter',
-      // TODO: Fill out the theme parameter
-      home: CategoryRoute(),
+      theme: ThemeData(
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: Colors.black,
+              displayColor: Colors.grey[600],
+            ),
+        // This colors the [InputOutlineBorder] when it is selected
+        primaryColor: Colors.grey[500],
+        textSelectionTheme:
+            TextSelectionThemeData(selectionHandleColor: Colors.green[500]),
+      ),
+      home: const CategoryRoute(),
     );
   }
 }
